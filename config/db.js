c64deacec964d31217c9fe21a827d049f959cd3f
Sarai Leon <sleon90@gmail.com>
2017-04-22 13:07:48 -0500

    add user & name database

--- config/db.js
+++ config/db.js
@@ -4,8 +4,10 @@ const chalk  	 = require('chalk');
 let   Mongoose = require('mongoose'),
   	  config 	 = require('./config');
 
+const connectUrl = 'mongodb://'+config.database.username+':'+config.database.password+'@' + config.database.host + ':' + config.database.port + '/' + config.database.db;
+
 Mongoose.Promise = global.Promise;
-Mongoose.connect('mongodb://' + config.database.host + '/' + config.database.db);
+Mongoose.connect(connectUrl);
 
 let db = Mongoose.connection;
 
